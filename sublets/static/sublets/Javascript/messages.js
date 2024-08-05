ec011e5f25e02aa23954e6fe1a97503246de100e
Tadhg Scannell <scannellstp@gmail.com>
2024-08-05 17:17:10 +0000

    Message Javascript WiP

--- sublets/static/sublets/Javascript/messages.js
+++ sublets/static/sublets/Javascript/messages.js
@@ -4,17 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#show-messages').style.display = 'block';
   document.querySelector('#respond').style.display = 'none';
 
-
-  // Use buttons to toggle between views
-  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
-  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
-  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
-  document.querySelector('#compose').addEventListener('click', compose_email);
-
-  // Select the two buttons for both the archive and respond button
-  message_button = document.querySelector(".archive_button");
-
-
   load_messages()
 
 
@@ -23,8 +12,20 @@ document.addEventListener('DOMContentLoaded', function() {
 // Reworked the compose function to take in arguments, used for the respond function.
 function load_messages() {
 
-    // Show compose view and hide other views
-    document.querySelector('#show-messages').style.display = 'block';
-    document.querySelector('#respond').style.display = 'none';
+  // Show compose view and hide other views
+  document.querySelector('#show-messages').style.display = 'block';
+  document.querySelector('#respond').style.display = 'none';
+
+  // Select the two buttons for both the archive and respond button
+  message_button = document.querySelector(".archive_button");
+
+  // loop through each button and add a click event listener
+  message_button.forEach(function(button) {
+            button.addEventListener("click", function() {
+              // do something when the button is clicked
+              alert(button.id)
+            });
+          });
+
 
 }
